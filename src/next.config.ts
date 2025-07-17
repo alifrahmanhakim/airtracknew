d3790d4408a710171aff21bb105cb4faf6abe44d
Taruna Rumput <tarunarumput@gmail.com>
2025-07-17 08:40:19 +0000

    saya curiga dengan cache terkait dengan agar web lebih responsif bisa ga

--- src/next.config.ts
+++ src/next.config.ts
@@ -18,9 +18,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  devServer: {
-    allowedDevOrigins: "https://*.cluster-ubrd2huk7jh6otbgyei4h62ope.cloudworkstations.dev",
-  }
 };
 
 export default nextConfig;
