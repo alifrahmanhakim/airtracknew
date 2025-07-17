f169492ec97ce8834e354878ba4f1e0fd82cbe0f
Taruna Rumput <tarunarumput@gmail.com>
2025-07-17 00:55:59 +0000

    Request URL https://firestore.googleapis.com/google.firestore.v1.Firesto

---
+++ src/next.config.ts
@@ -0,0 +1,26 @@
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  devServer: {
+    allowedDevOrigins: "https://*.cluster-ubrd2huk7jh6otbgyei4h62ope.cloudworkstations.dev",
+  }
+};
+
+export default nextConfig;
