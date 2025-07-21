62e376ae7a1c04208092fa9ec64910cbdd9ccbaa
Taruna Rumput <tarunarumput@gmail.com>
2025-07-21 11:50:53 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Button variant="o

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -41,7 +41,7 @@ export function ThemeToggle() {
           : "border-input bg-background"
       )}
     >
-      <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+      <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 text-orange-500" />
       <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
       <span className="sr-only">Toggle theme</span>
     </Button>
