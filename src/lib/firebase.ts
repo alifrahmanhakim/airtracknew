17324217c590b8a22a8d639ea07a735de5238f50
Taruna Rumput <tarunarumput@gmail.com>
2025-09-23 04:09:44 +0000

    ganti firebase config ke // Import the functions you need from the SDKs

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,20 +1,20 @@
-
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCVT1PDkGdczXUP_LatsS6Q4K1h0xvXeT0",
-    authDomain: "aoc-insight.firebaseapp.com",
-    projectId: "aoc-insight",
-    storageBucket: "aoc-insight.appspot.com",
-    messagingSenderId: "795850632942",
-    appId: "1:795850632942:web:e780d8191316b17c8651a8",
-    measurementId: "G-CNFZC766E2"
+  apiKey: "AIzaSyCqr_jQW1ZxMuBjLDmDsoSZA8RKF-kjHO0",
+  authDomain: "airtrack-c7979.firebaseapp.com",
+  projectId: "airtrack-c7979",
+  storageBucket: "airtrack-c7979.appspot.com",
+  messagingSenderId: "1090515897511",
+  appId: "1:1090515897511:web:40a0425c8ce80d70599f82",
+  measurementId: "G-DFBV3WYJ6M"
 };
 
 
@@ -24,6 +24,7 @@ const db = getFirestore(app);
 const storage = getStorage(app);
 const auth = getAuth(app);
 const googleProvider = new GoogleAuthProvider();
+// const analytics = getAnalytics(app);
 
 
 export { db, storage, app, auth, googleProvider };
