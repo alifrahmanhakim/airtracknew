60fc7c7e3c6ced9bc92bd0e15d6f82cba2899970
Taruna Rumput <tarunarumput@gmail.com>
2025-10-03 08:10:59 +0000

    tombol ini jebol ke border kanan card (_for element <Button>_)

--- src/components/tasks-table.tsx
+++ src/components/tasks-table.tsx
@@ -481,7 +481,7 @@ export function TasksTable({ projectId, projectType, tasks, teamMembers, onTasks
                             <ClipboardList /> Tasks
                         </CardTitle>
                     </div>
-                    <div className="flex flex-col sm:flex-row items-center gap-2 w-full md:w-auto">
+                    <div className="flex flex-col sm:flex-row flex-wrap items-center justify-end gap-2 w-full md:w-auto">
                         <div className="relative w-full sm:w-auto">
                             <Search className="absolute left-3 top-1/2 -translate-y-1/2 h-4 w-4 text-muted-foreground" />
                             <Input 
