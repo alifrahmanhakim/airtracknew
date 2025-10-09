00caf384956e3057768b76b1ae46fec3a70ffea0
Taruna Rumput <tarunarumput@gmail.com>
2025-10-09 11:17:01 +0000

    Try fixing this error: `Runtime Error: Error: allRecords is not defined.

--- src/app/(app)/rsi/monitoring-rekomendasi/page.tsx
+++ src/app/(app)/rsi/monitoring-rekomendasi/page.tsx
@@ -130,7 +130,7 @@ export default function MonitoringRekomendasiPage() {
     };
 
     const yearOptions = React.useMemo(() => ['all', ...[...new Set(records.map(r => r.tahun))].sort((a,b) => b - a)], [records]);
-    const penerimaOptions = React.useMemo(() => ['all', ...[...new Set(allRecords.map(r => r.operator || ''))].sort(), ...aocOptions.map(a => a.value)], [records]);
+    const penerimaOptions = React.useMemo(() => ['all', ...[...new Set(records.map(r => r.penerimaRekomendasi || ''))].sort(), ...aocOptions.map(a => a.value)], [records]);
 
 
     const filteredRecords = React.useMemo(() => {
