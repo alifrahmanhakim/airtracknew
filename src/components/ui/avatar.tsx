5625fbf4d5a2340d3fcdcc05f39fe3b43e1f344a
Taruna Rumput <tarunarumput@gmail.com>
2025-10-11 10:09:22 +0000

    yang tidak online kasih indicator abu abu

--- src/components/ui/avatar.tsx
+++ src/components/ui/avatar.tsx
@@ -18,10 +18,15 @@ const Avatar = React.forwardRef<
       )}
       {...props}
     />
-    {online && (
+    {online === true && (
        <span className="absolute bottom-0 right-0 flex h-2.5 w-2.5">
           <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
           <span className="relative inline-flex rounded-full h-2.5 w-2.5 bg-green-500 ring-1 ring-background"></span>
+        </span>
+    )}
+    {online === false && (
+         <span className="absolute bottom-0 right-0 flex h-2.5 w-2.5">
+          <span className="relative inline-flex rounded-full h-2.5 w-2.5 bg-gray-400 ring-1 ring-background"></span>
         </span>
     )}
   </div>
