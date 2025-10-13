fc23a94e74600e8547ded2678b4dc143c6bff917
Taruna Rumput <tarunarumput@gmail.com>
2025-10-13 13:49:54 +0000

    bar ini kasih outer biar kelihatan dan kasih efek loading ketika di hove

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -13,14 +13,18 @@ const Progress = React.forwardRef<
   <ProgressPrimitive.Root
     ref={ref}
     className={cn(
-      "relative h-4 w-full overflow-hidden rounded-full bg-secondary",
+      "relative h-4 w-full overflow-hidden rounded-full bg-secondary border group",
       className
     )}
     {...props}
   >
     <ProgressPrimitive.Indicator
-      className={cn("h-full w-full flex-1 bg-primary transition-all", indicatorClassName)}
-      style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
+      className={cn("h-full w-full flex-1 bg-primary transition-all group-hover:animate-progress-stripes", indicatorClassName)}
+      style={{ 
+        transform: `translateX(-${100 - (value || 0)}%)`,
+        backgroundImage: 'linear-gradient(45deg, hsla(0, 0%, 100%, .15) 25%, transparent 25%, transparent 50%, hsla(0, 0%, 100%, .15) 50%, hsla(0, 0%, 100%, .15) 75%, transparent 75%, transparent)',
+        backgroundSize: '1rem 1rem'
+      }}
     />
   </ProgressPrimitive.Root>
 ))
