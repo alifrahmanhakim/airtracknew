d08d5d076c9bc101f4fbf8a69cc01f5ff240ee44
Taruna Rumput <tarunarumput@gmail.com>
2025-10-18 04:55:00 +0000

    dia masih pakai favicon firebase padahal sudah saya buat favicon baru ht

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head />
       <body className={cn("min-h-screen font-sans antialiased", fontSans.variable)}>
         <ThemeProvider
           attribute="class"
