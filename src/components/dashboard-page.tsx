803e2c89f21ef3c33c6df3da14e8ff2b9b32fd6c
Subdit STD <riskregisterdkppu@gmail.com>
2025-10-19 01:59:48 +0000

    buat gambarnya tadi taruh pojok kanan lebih turun

--- src/components/dashboard-page.tsx
+++ src/components/dashboard-page.tsx
@@ -373,7 +373,7 @@ export function DashboardPage({ initialProjects, initialUsers }: DashboardPagePr
           style={{
             backgroundImage: "url('https://ik.imagekit.io/avmxsiusm/Screenshot%202025-10-19%20at%2008.40.28.png')",
             backgroundSize: '50%',
-            backgroundPosition: 'center',
+            backgroundPosition: 'bottom right',
           }}
         >
           <div className="absolute inset-0 bg-black/50 z-0"></div>
