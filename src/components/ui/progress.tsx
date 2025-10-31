4fd0a3ce4eb32a896ecd1cb6b92be6dbb748066d
Taruna Rumput <tarunarumput@gmail.com>
2025-10-31 15:23:57 +0000

    progress barnya kasih rule warna 40% kebawah merah 70% kebawah kuning 71

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -9,25 +9,36 @@ import { cn } from "@/lib/utils"
 const Progress = React.forwardRef<
   React.ElementRef<typeof ProgressPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root> & { indicatorClassName?: string }
->(({ className, value, indicatorClassName, ...props }, ref) => (
-  <ProgressPrimitive.Root
-    ref={ref}
-    className={cn(
-      "relative h-2 w-full overflow-hidden rounded-full bg-secondary border group",
-      className
-    )}
-    {...props}
-  >
-    <ProgressPrimitive.Indicator
-      className={cn("h-full w-full flex-1 bg-primary transition-all group-hover:animate-progress-stripes", indicatorClassName)}
-      style={{ 
-        transform: `translateX(-${100 - (value || 0)}%)`,
-        backgroundImage: 'linear-gradient(45deg, hsla(0, 0%, 100%, .15) 25%, transparent 25%, transparent 50%, hsla(0, 0%, 100%, .15) 50%, hsla(0, 0%, 100%, .15) 75%, transparent 75%, transparent)',
-        backgroundSize: '1rem 1rem'
-      }}
-    />
-  </ProgressPrimitive.Root>
-))
+>(({ className, value, indicatorClassName, ...props }, ref) => {
+  const progressValue = value || 0;
+  
+  const colorClass =
+    progressValue <= 40
+      ? "bg-red-500"
+      : progressValue <= 70
+      ? "bg-yellow-500"
+      : "bg-green-500";
+
+  return (
+    <ProgressPrimitive.Root
+      ref={ref}
+      className={cn(
+        "relative h-2 w-full overflow-hidden rounded-full bg-secondary border group",
+        className
+      )}
+      {...props}
+    >
+      <ProgressPrimitive.Indicator
+        className={cn("h-full w-full flex-1 transition-all group-hover:animate-progress-stripes", colorClass, indicatorClassName)}
+        style={{ 
+          transform: `translateX(-${100 - (value || 0)}%)`,
+          backgroundImage: 'linear-gradient(45deg, hsla(0, 0%, 100%, .15) 25%, transparent 25%, transparent 50%, hsla(0, 0%, 100%, .15) 50%, hsla(0, 0%, 100%, .15) 75%, transparent 75%, transparent)',
+          backgroundSize: '1rem 1rem'
+        }}
+      />
+    </ProgressPrimitive.Root>
+  )
+})
 Progress.displayName = ProgressPrimitive.Root.displayName
 
 export { Progress }
